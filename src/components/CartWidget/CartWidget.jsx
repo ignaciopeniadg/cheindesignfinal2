ed34957df579a82d85a1abbda9d53f61f50e5f79
ignaciopeniadg <ignaciopeniadg@gmail.com>
2023-07-30 13:13:18 -0300

    css al cartwidget

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 import { NavLink } from 'react-router-dom'
 import { useCartContext } from '../../context/CartContext'
+import './cartwidget.css'
 
 export const CartWidget = () => {
     const { totalProducts } = useCartContext();
     return (
-        <>
+        <div className='cartwidget'>
             <NavLink to='/carrito'><AiOutlineShoppingCart /></NavLink>
             <span>{totalProducts() || ''}</span>
-        </>
+        </div>
 
     )
 }
